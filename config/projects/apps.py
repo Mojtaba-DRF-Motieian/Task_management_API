e9257c5fd8175991819a52276d5887add35f5d6c
mojtaba motieian <mojtabamotiaian@gmail.com>
2025-12-22 20:51:59 +0330

    add projects app base

--- config/projects/apps.py
+++ config/projects/apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 
 
-class ProejctsConfig(AppConfig):
+class ProjectsConfig(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
-    name = "proejcts"
+    name = "projects"
